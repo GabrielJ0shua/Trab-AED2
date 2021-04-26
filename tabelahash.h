ecfa1e7d8ea25a2aaa5ae46f56ed79fb83e626c1
matrocheetos <58711427+matrocheetos@users.noreply.github.com>
2021-04-26 16:50:45 -0300

    correções e espaçamento

--- tabelahash.h
+++ tabelahash.h
@@ -13,16 +13,8 @@ int chaveDobra(int chave, int TABLE_SIZE);
 int valorString(char*str);
 int sondagemLinear(int pos, int i, int TABLE_SIZE);
 int sondagemQuadratica(int pos, int i, int TABLE_SIZE);
- int duploHash(int H1, int chave, int i, int TABLE_SIZE);
+int duploHash(int H1, int chave, int i, int TABLE_SIZE);
 int insereHash_semColisao(Hash* ha, struct aluno al);
 int buscaHash_SemColisao(Hash* ha, int mat,struct aluno* al);
 int insereHash_EnderAberto(Hash* ha, struct aluno al);
 int buscaHash_EnderAberto(Hash* ha,int mat, struct aluno* al);
-
-{
-    /* data */
-};
-)
-{
-    /* data */
-};
